51a26cc424492ec44a1bdb212dd756245d7d2120
Kartik <kartikagarwal20144@gmail.com>
2024-06-16 11:47:27 +0530

    removing bug

--- config/db.js
+++ config/db.js
@@ -6,8 +6,7 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
   }
 };
 
-export default connectDB;
+export default connectDB
